7ee12cfa04e58dba08e80a1ba99ad02db81b4b16
Your Name <aharon@ald.co.il>
2019-01-11 00:37:20 +0200

    Statistics

--- SmartList/src/app/components/groups-checklists/groups-checklists.component.ts
+++ SmartList/src/app/components/groups-checklists/groups-checklists.component.ts
@@ -31,26 +31,15 @@ export class GroupsChecklistsComponent implements OnInit {
   generateData() {
     this.chartData = [];
     this.groupsService.getGroupChecklistsCount().subscribe(groupData => {
-      //this.GroupsCount =JSON.stringify(groupData );
-      //return { x: groupData._id, y: groupData.count };
-      
-      //console.log(groupData._id);
-      //console.log(groupData.count);
-      //console.log(groupData  );
-
-      //console.log(groupData);
-
-
-      const userStr = JSON.stringify(groupData);
-
+      const groups = JSON.stringify(groupData);
       var arr:Array<any>;
-      arr = JSON.parse(userStr, (key, value) => {
+      arr = JSON.parse(groups, (key, value) => {
         if (typeof value === 'string') {
           return value.toUpperCase();
         }
         return value;
       });
-      console.log(arr);
+      //console.log(arr);
 
 
       this.chartData = [];
@@ -63,12 +52,6 @@ export class GroupsChecklistsComponent implements OnInit {
         ]);
       }
 
-//      arr.forEach(item => {
-//        this.chartData.push([item["count"],item["_id"]]);      
-//      });
-      //this.chartData.push([groupData._id,groupData.count]);      
-      console.log( this.chartData);
-      //console.log(this.chartData[this.chartData.length-1,1]);
      })
 /*
     this.chartData = [];
@@ -82,55 +65,3 @@ export class GroupsChecklistsComponent implements OnInit {
     */
   }
 }
-/*
-import { Component, OnInit } from '@angular/core';
-import { AreaChartConfig } from '../area-chart/area-chart-config';
-import { GroupsHLLService } from "../../services/groups-hll.service";
-
-@Component({
-  selector: 'app-groups-checklists',
-  templateUrl: './groups-checklists.component.html',
-  styleUrls: ['./groups-checklists.component.css']
-})
-
-export class GroupsChecklistsComponent implements OnInit {
-
-  private areaChartConfig: Array<AreaChartConfig>;
-
-  constructor(    public groupsService: GroupsHLLService) { 
-
-    
-  }
-
-  ngOnInit() {
-  }
-
-  getStats() {
-*/
-
-    /*
-// We create a new AreaChartConfig object to set groups by checklists config
-let groupsArea = {
-  settings: {
-    fill: 'rgba(195, 0, 47, 1)',
-    interpolation: 'monotone'
-  }, dataset: this.groupsService.getGroupChecklistsCount().subscribe(groupData => {
-    console.log(groupData);
-    //this.GroupsCount =parseInt(JSON.stringify(groupData) );
-    return { x: groupData._id, y: groupData.count };
-   })
-   //.map(groupData => {
-   // return { x: groupData._id, y: groupData.count };
-  //})
-};
-
-// to finish we append our AreaChartConfigs into an array of configs 
-this.areaChartConfig = new Array<AreaChartConfig>();
-this.areaChartConfig.push(groupsArea);
-
-   */
-
-   /*
-  }
-}
-*/
